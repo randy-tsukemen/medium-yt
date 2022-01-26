be89ccab5571a2ac0e1cbc59e0b2f4b3aad1aa57
Randy, Huang <gg875437@gmail.com>
2022-01-26 17:48:05 +0900

    create the post parts and layout

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,5 @@
 import Head from "next/head";
+import Link from "next/link";
 import Header from "../components/Header";
 import { sanityClient, urlFor } from "../sanity";
 import { Post } from "../typings";
@@ -39,6 +40,38 @@ export default function Home({ posts }: Props) {
           src="https://accountabilitylab.org/wp-content/uploads/2020/03/Medium-logo.png"
           alt=""
         />
+      </div>
+
+      {/* Posts */}
+      <div
+        className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-3
+      md:gap-6 p-2 md:p-6"
+      >
+        {posts.map((post) => (
+          <Link key={post._id} href={`/posts/${post.slug.current}`}>
+            <div className="group cursor-pointer border rounded-lg overflow-hidden">
+              <img
+                className="h-60 w-full object-cover group-hover:scale-105
+                transition-transform duration-200 ease-in-out"
+                src={urlFor(post.mainImage).url()!}
+                alt=""
+              />
+              <div className="flex justify-between p-5 bg-white">
+                <div>
+                  <p className="text-lg font-bold">{post.title}</p>
+                  <p className="text-xs">
+                    {post.description} by {post.author.name}
+                  </p>
+                </div>
+                <img
+                  className="h-12 w-12 rounded-full"
+                  src={urlFor(post.author.image).url()!}
+                  alt=""
+                />
+              </div>
+            </div>
+          </Link>
+        ))}
       </div>
     </div>
   );
