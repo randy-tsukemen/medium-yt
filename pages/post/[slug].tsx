2adc7625a9030f9be43d5a8bff3849852cc22648
Randy, Huang <gg875437@gmail.com>
2022-01-27 00:42:27 +0900

    create reply placeholder

--- pages/post/[slug].tsx
+++ pages/post/[slug].tsx
@@ -64,6 +64,42 @@ const Post = ({ post }: Props) => {
           />
         </div>
       </article>
+
+      <hr className="max-w-lg my-5 mx-auto border border-yellow-500" />
+
+      <form className="flex flex-col p-5 max-w-2xl mx-auto mb-10" action="">
+        <h3 className="text-sm text-yellow-500">Enjoyed this article?</h3>
+        <h4 className="text-3xl font-bold">Leave a comment below!</h4>
+        <hr className="py-3 mt-2" />
+
+        <label className="block mb-5" htmlFor="">
+          <span className="text-gray-700">Name</span>
+          <input
+            className="shadow border rounded py-2 px-3
+              mt-1 block w-full focus:outline-none focus:border-yellow-500"
+            placeholder="John Appleseed"
+            type="text"
+          />
+        </label>
+        <label className="block mb-5" htmlFor="">
+          <span className="text-gray-700">Email</span>
+          <input
+            className="shadow border rounded py-2 px-3  mt-1 block
+              w-full focus:outline-none focus:border-yellow-500"
+            placeholder="John Appleseed"
+            type="text"
+          />
+        </label>
+        <label className="block mb-5" htmlFor="">
+          <span className="text-gray-700">Comment</span>
+          <textarea
+            className="shadow border rounded py-2 px-3
+              mt-1 block w-full focus:outline-none focus:border-yellow-500"
+            placeholder="John Appleseed"
+            rows={8}
+          />
+        </label>
+      </form>
     </main>
   );
 };
