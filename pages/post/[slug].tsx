e2790875115e5c3f2fc49b2b8de1940ede2693f3
Randy, Huang <gg875437@gmail.com>
2022-01-29 17:20:25 +0900

    show  comments

--- pages/post/[slug].tsx
+++ pages/post/[slug].tsx
@@ -181,6 +181,23 @@ const Post = ({ post }: Props) => {
           />
         </form>
       )}
+
+      {/* Comments */}
+      <div
+        className="flex flex-col p-10 my-10 max-w-2xl mx-auto
+        shadow-yellow-500 shadow space-y-2"
+      >
+        <h3 className="text-3xl font-bold">Comments</h3>
+        <hr className="pb-2" />
+        {post.comments.map((comment) => (
+          <div key={comment._id}>
+            <p>
+              <span className="text-yellow-500">{comment.name}: </span>
+              {comment.comment}
+            </p>
+          </div>
+        ))}
+      </div>
     </main>
   );
 };
@@ -218,6 +235,11 @@ export const getStaticProps: GetStaticProps = async ({ params }) => {
       name,
       image
   },
+  'comments': *[
+    _type == "comment" &&
+    post._ref == ^._id &&
+    approved == true
+  ],
     description,
     mainImage,
     slug,
